f92ef87fc77d579513014c9eb29aaba6cdcaf335
sameh-amr <samehamrsw@gmail.com>
2025-09-29 15:59:12 +0300

    persistence: register FavoriteEntity and bind FavoriteRepository

--- src/infrastructure/persistence/persistence.module.ts
+++ src/infrastructure/persistence/persistence.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MovieEntity } from './entities/movie.entity';
 import { GenreEntity } from './entities/genre.entity';
 import { RatingEntity } from './entities/rating.entity';
+import { FavoriteEntity } from './entities/favorite.entity';
+
 
 import { TypeormMovieRepository } from './repositories/typeorm-movie.repository';
 import { TypeormGenreRepository } from './repositories/typeorm-genre.repository';
@@ -14,14 +16,18 @@ import { GenreRepository } from '../../domain/repositories/genre.repository';
 import { RatingRepository } from 'src/domain/repositories/rating.repository';
 import { TypeormRatingRepository } from './repositories/typeorm-rating.repository';
 
+import { FavoriteRepository } from '../../domain/repositories/favorite.repository';
+import { TypeormFavoriteRepository } from './repositories/typeorm-favorite.repository';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieEntity, GenreEntity,RatingEntity])],
+  imports: [TypeOrmModule.forFeature([MovieEntity, GenreEntity,RatingEntity,FavoriteEntity])],
   providers: [
     { provide: MovieRepository, useClass: TypeormMovieRepository },
     { provide: GenreRepository, useClass: TypeormGenreRepository },
     { provide: RatingRepository, useClass: TypeormRatingRepository },
+    { provide: FavoriteRepository, useClass: TypeormFavoriteRepository  },
   ],
-  exports: [MovieRepository, GenreRepository, RatingRepository],
+  exports: [MovieRepository, GenreRepository, RatingRepository,FavoriteRepository],
 })
 export class PersistenceModule {}
