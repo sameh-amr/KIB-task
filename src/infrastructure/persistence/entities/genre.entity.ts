38d5484ffe5c5e59d04ea4a8e379c939019eb0e8
sameh-amr <samehamrsw@gmail.com>
2025-09-29 14:36:04 +0300

    infra(genre): add tmdbId column and unique index to GenreEntity

--- src/infrastructure/persistence/entities/genre.entity.ts
+++ src/infrastructure/persistence/entities/genre.entity.ts
@@ -1,10 +1,13 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('genres')
 export class GenreEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Index({ unique: true })
+  @Column({ type: 'int', nullable: true }) // nullable for smooth migration; we’ll enforce uniqueness where not null
+  tmdbId: number | null;
   @Column({ type: 'varchar', length: 120, unique: true })
   name: string;
 }
