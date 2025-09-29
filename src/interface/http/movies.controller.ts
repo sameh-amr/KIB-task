e07e6d275c08b44bdfb77884c8b7c20a3ce647fd
sameh-amr <samehamrsw@gmail.com>
2025-09-29 15:18:35 +0300

    http: accept q/genreId/page/limit on GET /movies and pass to ListMoviesQuery

--- src/interface/http/movies.controller.ts
+++ src/interface/http/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, NotFoundException, Query } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { ListMoviesQuery } from '../../application/queries/movies/list-movies/list-movies.query';
 import { GetMovieQuery } from '../../application/queries/movies/get-movie/get-movie.query';
@@ -8,8 +8,16 @@ export class MoviesController {
   constructor(private readonly queryBus: QueryBus) {}
 
   @Get()
-  async list() {
-    return this.queryBus.execute(new ListMoviesQuery());
+  async list(
+    @Query('q') q?: string,
+    @Query('genreId') genreIdRaw?: string,
+    @Query('page') pageRaw?: string,
+    @Query('limit') limitRaw?: string,
+  ) {
+    const genreId = genreIdRaw ? Number(genreIdRaw) : undefined;
+    const page = pageRaw ? Number(pageRaw) : undefined;
+    const limit = limitRaw ? Number(limitRaw) : undefined;
+    return this.queryBus.execute(new ListMoviesQuery(q, genreId, page, limit));
   }
 
   @Get(':id')
