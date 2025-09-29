28e62bac016a80c06bca0c764c031b6372e32138
sameh-amr <samehamrsw@gmail.com>
2025-09-29 12:20:23 +0300

    register movie query handlers in ApplicationModule

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ListGenresHandler } from './queries/genres/list-genres/list-genres.handler';
+import { ListMoviesHandler } from './queries/movies/list-movies/list-movies.handler';
+import { GetMovieHandler } from './queries/movies/get-movie/get-movie.handler';
 import { PersistenceModule } from '../infrastructure/persistence/persistence.module';
 
 @Module({
   imports: [CqrsModule, PersistenceModule],
-  providers: [ListGenresHandler],
+  providers: [ListGenresHandler, ListMoviesHandler, GetMovieHandler],
   exports: [CqrsModule],
 })
 export class ApplicationModule {}
