0083db66bb534ec243ec830ae835d8fadc85b5e0
sameh-amr <samehamrsw@gmail.com>
2025-09-29 14:49:48 +0300

    domain(movie): add setGenres(movieId, genreIds) to repository contract

--- src/domain/repositories/movie.repository.ts
+++ src/domain/repositories/movie.repository.ts
@@ -4,4 +4,5 @@ import { BaseRepository } from './base.repository';
 
 export abstract class MovieRepository extends BaseRepository<Movie, number,NewMovie> {
   abstract findByTmdbId(tmdbId: number): Promise<Movie | null>;
+   abstract setGenres(movieId: number, genreIds: number[]): Promise<void>;
 }
