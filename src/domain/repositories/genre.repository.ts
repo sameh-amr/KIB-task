a57aa0ba333e0826a6c9b8fd510c3f02f9660242
sameh-amr <samehamrsw@gmail.com>
2025-09-29 13:06:20 +0300

    added the tyope to our layer

--- src/domain/repositories/genre.repository.ts
+++ src/domain/repositories/genre.repository.ts
@@ -1,6 +1,7 @@
 import type { Genre } from '../models/genre';
+import { NewGenre } from '../models/new-genre';
 import { BaseRepository } from './base.repository';
 
-export abstract class GenreRepository extends BaseRepository<Genre, number> {
+export abstract class GenreRepository extends BaseRepository<Genre, number,NewGenre> {
   abstract findByName(name: string): Promise<Genre | null>;
 }
--- src/domain/repositories/movie.repository.ts
+++ src/domain/repositories/movie.repository.ts
@@ -1,6 +1,7 @@
 import type { Movie } from '../models/movie';
+import { NewMovie } from '../models/new-movie';
 import { BaseRepository } from './base.repository';
 
-export abstract class MovieRepository extends BaseRepository<Movie, number> {
+export abstract class MovieRepository extends BaseRepository<Movie, number,NewMovie> {
   abstract findByTmdbId(tmdbId: number): Promise<Movie | null>;
 }
