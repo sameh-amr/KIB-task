9e4303b5bfe77ecbba3b185c831bbf15d865ce27
sameh-amr <samehamrsw@gmail.com>
2025-09-29 15:49:09 +0300

    http(rating): add POST /movies/:id/ratings to rate a movie and return new average

--- src/interface/http/movies.controller.ts
+++ src/interface/http/movies.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Param, ParseIntPipe, NotFoundException, Query } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
+import { Controller, Get, Param, ParseIntPipe, NotFoundException, Query, Post, Body, BadRequestException } from '@nestjs/common';
+import { QueryBus, CommandBus } from '@nestjs/cqrs';
 import { ListMoviesQuery } from '../../application/queries/movies/list-movies/list-movies.query';
 import { GetMovieQuery } from '../../application/queries/movies/get-movie/get-movie.query';
+import { RateMovieCommand } from '../../application/commands/ratings/rate-movie/rate-movie.command';
+
+type RateBody = { userId: string; rating: number };
 
 @Controller('movies')
 export class MoviesController {
-  constructor(private readonly queryBus: QueryBus) {}
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: CommandBus,
+  ) {}
 
   @Get()
   async list(
@@ -25,5 +31,17 @@ export class MoviesController {
     const movie = await this.queryBus.execute(new GetMovieQuery(id));
     if (!movie) throw new NotFoundException('Movie not found');
     return movie;
+  }
+
+  @Post(':id/ratings')
+  async rate(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: RateBody,
+  ) {
+    if (!body?.userId || typeof body.userId !== 'string') {
+      throw new BadRequestException('userId is required');
+    }
+    const rating = Number((body as any).rating);
+    return this.commandBus.execute(new RateMovieCommand(id, body.userId, rating));
   }
 }
