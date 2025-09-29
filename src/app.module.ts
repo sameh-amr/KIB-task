531251341942883f59608d0763164fb1abf041cd
sameh-amr <samehamrsw@gmail.com>
2025-09-29 12:04:47 +0300

    add PersistenceModule and register it in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersistenceModule } from './infrastructure/persistence/persistence.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // reads .env
+    ConfigModule.forRoot({ isGlobal: true }), 
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
@@ -19,6 +20,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: false, 
       }),
     }),
+    PersistenceModule
   ],
   controllers: [AppController],
   providers: [AppService],
