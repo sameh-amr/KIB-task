f3d41d38a991243f08ff46f7021363dbbf2d7c5a
sameh-amr <samehamrsw@gmail.com>
2025-09-29 19:20:54 +0300

    perf(cache): update user favorites caches in-place on remove (no global bust)

--- src/application/commands/favorites/remove-favorite/remove-favorite.handler.ts
+++ src/application/commands/favorites/remove-favorite/remove-favorite.handler.ts
@@ -1,13 +1,20 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { RemoveFavoriteCommand } from './remove-favorite.command';
 import { FavoriteRepository } from '../../../../domain/repositories/favorite.repository';
+import { MoviesCacheService } from 'src/infrastructure/cache/movies-cache.service';
 
 @CommandHandler(RemoveFavoriteCommand)
-export class RemoveFavoriteHandler implements ICommandHandler<RemoveFavoriteCommand> {
-  constructor(private readonly favorites: FavoriteRepository) {}
+export class RemoveFavoriteHandler
+  implements ICommandHandler<RemoveFavoriteCommand>
+{
+  constructor(
+    private readonly favorites: FavoriteRepository,
+    private readonly cache: MoviesCacheService,
+  ) {}
 
   async execute({ userId, movieId }: RemoveFavoriteCommand) {
     await this.favorites.deleteByUserAndMovie(userId, movieId);
+    await this.cache.updateFavoritesAfterRemove(userId, movieId);
     return { ok: true };
   }
 }
